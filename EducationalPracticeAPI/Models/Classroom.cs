481c9b740ba8d94711e2e2f12083f7527a9a29e2
Владислав Сибиркин <balatralba@gmail.com>
2026-01-15 01:19:11 +0700

    Описание сущности "Класс"

--- EducationalPracticeAPI/Models/Classroom.cs
+++ EducationalPracticeAPI/Models/Classroom.cs
@@ -1,6 +1,23 @@
-﻿namespace EducationalPracticeAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EducationalPracticeAPI.Models
 {
+    [Table("classroom")]
     public class Classroom
     {
+        [Key]
+        [Column("classroom_id")]
+        public int ClassroomId { get; set; }
+
+        [Column("building_id")]
+        public int BuildingId { get; set; }
+
+        [ForeignKey("BuildingId")]
+        public Building Building { get; set; } = null!;
+
+        [Column("room_number")]
+        [Required]
+        public string RoomNumber { get; set; } = null!;
     }
 }

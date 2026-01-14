51e2e94ab051636b40a23509607f713a9cc117e3
Владислав Сибиркин <balatralba@gmail.com>
2026-01-15 01:05:41 +0700

    Описание сущности "Строение"

--- EducationalPracticeAPI/Models/Building.cs
+++ EducationalPracticeAPI/Models/Building.cs
@@ -1,6 +1,23 @@
-﻿namespace EducationalPracticeAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EducationalPracticeAPI.Models
 {
+    [Table("building")]
     public class Building
     {
+        [Key]
+        [Column("building_id")]
+        public int BuildingId { get; set; }
+
+        [Column("name")]
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Column("address")]
+        [Required]
+        public string Address { get; set; } = null!;
+
+        public List<Classroom> Classrooms { get; set; } = new();
     }
 }

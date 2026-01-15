b5c46757ea782e1f456d9438d54eccbc868f4168
Владислав Сибиркин <balatralba@gmail.com>
2026-01-15 08:58:23 +0700

    Описание сущности "Предмет"

--- EducationalPracticeAPI/Models/Subject.cs
+++ EducationalPracticeAPI/Models/Subject.cs
@@ -1,6 +1,16 @@
-﻿namespace EducationalPracticeAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EducationalPracticeAPI.Models
 {
+    [Table("subject")]
     public class Subject
     {
+        [Key]
+        [Column("subject_id")]
+        public int SubjectId { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; } = null!;
     }
 }

07a327d0286aab5dfe69c56f49c4c7876e3e5ffa
Владислав Сибиркин <balatralba@gmail.com>
2026-01-15 08:43:30 +0700

    Описание сущности "Специальность"

--- EducationalPracticeAPI/Models/Specialty.cs
+++ EducationalPracticeAPI/Models/Specialty.cs
@@ -1,6 +1,17 @@
-﻿namespace EducationalPracticeAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EducationalPracticeAPI.Models
 {
+    [Table("specialties")]
     public class Specialty
     {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        [Required]
+        public string Name { get; set; } = null!;
     }
 }

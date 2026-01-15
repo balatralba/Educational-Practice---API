b22198d4e87ce3ac2758ff39492a059115510a47
Владислав Сибиркин <balatralba@gmail.com>
2026-01-15 09:01:57 +0700

    Описание сущности "День недели"

--- EducationalPracticeAPI/Models/Weekday.cs
+++ EducationalPracticeAPI/Models/Weekday.cs
@@ -1,6 +1,17 @@
-﻿namespace EducationalPracticeAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EducationalPracticeAPI.Models
 {
+    [Table("weekday")]
     public class Weekday
     {
+        [Key]
+        [Column("weekday_id")]
+        public int WeekdayId { get; set; }
+
+        [Column("name")]
+        [Required]
+        public string Name { get; set; } = null!;
     }
 }

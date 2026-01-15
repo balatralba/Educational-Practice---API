de690e322685011d2170807504e8c0010eb7304e
Владислав Сибиркин <balatralba@gmail.com>
2026-01-15 08:55:55 +0700

    Описание сущности "Преподаватель"

--- EducationalPracticeAPI/Models/Teacher.cs
+++ EducationalPracticeAPI/Models/Teacher.cs
@@ -1,6 +1,29 @@
-﻿namespace EducationalPracticeAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EducationalPracticeAPI.Models
 {
+    [Table("teacher")]
     public class Teacher
     {
+        [Key]
+        [Column("teacher_id")]
+        public int TeacherId { get; set; }
+
+        [Column("last_name")]
+        [Required]
+        public string LastName { get; set; } = null!;
+
+        [Column("first_name")]
+        [Required]
+        public string FirstName { get; set; } = null!;
+
+        [Column("middle_name")]
+        [Required]
+        public string? MiddleName { get; set; }
+
+        [Column("position")]
+        [Required]
+        public string Position { get; set; } = null!;
     }
 }

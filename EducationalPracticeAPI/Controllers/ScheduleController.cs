b407cc02ad27b13e02627a621cbbb50c1d1f087d
Владислав Сибиркин <balatralba@gmail.com>
2026-01-15 15:55:30 +0700

    Создание API-контроллера

---
+++ EducationalPracticeAPI/Controllers/ScheduleController.cs
@@ -0,0 +1,19 @@
+﻿using EducationalPracticeAPI.Data;
+using EducationalPracticeAPI.Models;
+using EducationalPracticeAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EducationalPracticeAPI.Controllers
+{
+    [ApiController]
+    [Route("api/schedule")]
+    public class ScheduleController : ControllerBase
+    {
+        private readonly IScheduleService _service;
+        public ScheduleController(IScheduleService service, AppDbContext db)
+        {
+            _service = service;
+        }
+    }
+}
